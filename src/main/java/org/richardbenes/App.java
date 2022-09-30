dba1922b0795298a1a3291a8b56182a1f1bf4fbd
Richard Beneš <richard.benes@monster.com>
2022-09-30 16:27:49 +0200

    Initial commit - executable with exec:java

---
+++ src/main/java/org/richardbenes/App.java
@@ -0,0 +1,13 @@
+package org.richardbenes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/richardbenes/AppTest.java
@@ -0,0 +1,14 @@
+package org.richardbenes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+
+        assertThat(1 + 1).isEqualTo(2);
+    }
+}
