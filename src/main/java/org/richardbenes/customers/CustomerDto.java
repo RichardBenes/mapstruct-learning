a76b7dc2accd8bdc6073c55a82c39728480e8ae3
Richard Beneš <richard.benes@monster.com>
2022-10-03 12:34:37 +0200

    3.7 - Mapstruct using direct field access (no getters/setters)

---
+++ src/main/java/org/richardbenes/customers/CustomerDto.java
@@ -0,0 +1,16 @@
+package org.richardbenes.customers;
+
+import lombok.ToString;
+
+@ToString
+public class CustomerDto {
+
+    public CustomerDto(long id, String name) {
+        System.out.println("CustomerDto");
+        this.id = id;
+        this.name = name;
+    }
+
+    public long id;
+    public String name;
+}
