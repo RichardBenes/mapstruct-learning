181082668ba6b8a3b1afca29620f294d7d9a280d
Richard Beneš <richard.benes@monster.com>
2022-10-03 13:54:01 +0200

    3.10 - mapping from Map<String, String> to an Object

---
+++ src/main/java/org/richardbenes/phones/Phone.java
@@ -0,0 +1,10 @@
+package org.richardbenes.phones;
+
+import lombok.Data;
+
+@Data
+public class Phone {
+
+    private final String color;
+    private final int weight;
+}
--- src/main/java/org/richardbenes/stones/Stone.java
+++ src/main/java/org/richardbenes/stones/Stone.java
@@ -6,17 +6,17 @@ import lombok.Data;
 @Data
 public class Stone {
 
-//    public Stone() {
-//        System.out.println("new Stone()");
-//    }
+    public Stone() {
+        System.out.println("new Stone()");
+    }
 
     public Stone(int weight, int maxDimension) {
         System.out.println("new Stone(int, int)");
         this.weight = weight;
         this.maxDimension = maxDimension;
     }
-    
-    protected Stone(String weight, String maxDimension) {
+
+    public Stone(String weight, String maxDimension) {
         System.out.println("new Stone(String, String)");
         this.weight = Integer.parseInt(weight);
         this.maxDimension = Integer.parseInt(maxDimension);
