afad628d260d396ee00a9a24fa6b71ba36dbc0e9
Richard Beneš <richard.benes@monster.com>
2022-10-03 13:36:49 +0200

    3.9 - parameterless constructors have absolute priority

---
+++ src/main/java/org/richardbenes/stones/Default.java
@@ -0,0 +1,4 @@
+package org.richardbenes.stones;
+
+public @interface Default {
+}
---
+++ src/main/java/org/richardbenes/stones/StoneDto.java
@@ -0,0 +1,10 @@
+package org.richardbenes.stones;
+
+import lombok.Data;
+
+@Data
+public class StoneDto {
+
+    private final int weight;
+    private final int maxDimension;
+}
